0c3ab8d8b6d12cb90166a21ef66b50b28aa4c4fb
Shahin Jafarzadeh <shahin.jafarzadeh@qub.ac.uk>
2026-03-13 10:09:50 +0100

    Initialize WaLSA-Pipelines package
    Add __init__.py for WaLSA-Pipelines package with imports

---
+++ src/walsa_pipelines/__init__.py
@@ -0,0 +1,10 @@
+"""
+WaLSA-Pipelines package.
+
+High-level imports are re-exported here for convenience:
+    from WaLSA_pipelines import linefit
+"""
+
+from . import linefit
+
+__all__ = ["linefit"]
